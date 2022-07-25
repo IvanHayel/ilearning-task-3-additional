c3b3ef21492e079fd7784a42a0696d9becb8ff23
Ivan Hayel <i.hayel.dev@gmail.com>
2022-07-25 22:13:47 +0300

    Thinking how to bypass the slash in dotnet...

--- Task3.java
+++ Task3.java
@@ -1,10 +1,8 @@
-// \u000d\u002f\u002a
-    System.Console.WriteLine("Itransition");
-    // OR FOR JS:
-    // console.log("Itransition");
-// \u000d\u002a\u002f
 // \u000d public class Task3 {
 // \u000d  public static void main(String[] args) {
-// \u000d    System.out.println("Itransition");
+// \u000d    System.out.println("test");
 // \u000d  }
 // \u000d}
+// \u000d\u002f\u002a
+/System.Console.WriteLine("test");// console.log("test");
+// \u000d\u002a\u002f
