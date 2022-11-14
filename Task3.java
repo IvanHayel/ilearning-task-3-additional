e5266eb3c8de9836458b4beb69fd300d06ca9dab
Ivan Hayel <i.hayel.dev@gmail.com>
2022-11-14 23:15:23 +0300

    Solution presented.

--- Task3.java
+++ Task3.java
@@ -1,8 +1,15 @@
 // \u000d public class Task3 {
 // \u000d  public static void main(String[] args) {
-// \u000d    System.out.println("test");
+// \u000d    System.out.println("java");
 // \u000d  }
 // \u000d}
 // \u000d\u002f\u002a
-/System.Console.WriteLine("test");// console.log("test");
+var dynamic = 0;
+dynamic
+eval;
+if (0 == '0') {
+  eval("console.log('js');");
+  return;
+}
+System.Console.WriteLine("c#");
 // \u000d\u002a\u002f
